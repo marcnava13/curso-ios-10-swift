5fd111bbcf3f71881e92601c632d187f7459e027
Marcos Navarro <marcosnpcogollos@gmail.com>
2017-09-16 19:22:55 +0200

    My first playground in xCode and exercise the vars and constants

---
+++ Vars.playground/Contents.swift
@@ -0,0 +1,34 @@
+var gretting: String = "Hola"
+
+var name: String = "Marcos Navarro"
+
+var age: Int = 27
+
+gretting = "Hola \(name), tengo \(age) años"
+
+gretting
+
+let PI: Float = 3.14159265
+
+var birthDate: Int = 1990 // Variable
+
+let birthYear: Int = 1990 // Constante
+
+var text: String = "El tiempo está "
+
+var weather: String = "Soleado"
+
+text + weather
+
+weather
+
+weather = "LLuvioso"
+
+text + weather
+
+// Variables inicializadas como valor vacío
+var temperature: Float
+
+// Una constante siempre ha de estar inicializada, de lo contrario dará error
+let grativy: Float = 9.81
+
